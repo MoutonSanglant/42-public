3e9f2ab7ec5af19dcd67b027e782e01d42ff02a5
MoutonSanglant <defresne.thierry@gmail.com>
2015-09-19 22:11:16 +0200

    colle00

---
+++ Piscine C/srcs/ex00/colle00.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colle00.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpain <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/09/06 13:29:07 by lpain             #+#    #+#             */
+/*   Updated: 2015/09/06 18:17:03 by lpain            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_draw_square(int x, int y, int i, int j)
+{
+	if (j == 1 && (i == 1 || i == y))
+		ft_putchar('o');
+	else if (j > 1 && j < x && (i == 1 || i == y))
+		ft_putchar('-');
+	else if (j == x && (i == 1 || i == y))
+		ft_putchar('o');
+	else if (i > 1 && i < y && !(j > 1 && j < x))
+		ft_putchar('|');
+	else if (i > 1 && i < y && (j > 1 || j < x))
+		ft_putchar(' ');
+	if (j == x)
+		ft_putchar('\n');
+}
+
+void	colle(int x, int y)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 1;
+	while (i <= y)
+	{
+		while (j <= x)
+		{
+			ft_draw_square(x, y, i, j);
+			j++;
+		}
+		j = 1;
+		i++;
+	}
+}
---
+++ Piscine C/srcs/ex00/colle03.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colle03.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/09/06 21:50:59 by tdefresn          #+#    #+#             */
+/*   Updated: 2015/09/06 22:05:25 by tdefresn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_draw_square(int x, int y, int i, int j)
+{
+	if (i == 1 && j == 1)
+		ft_putchar('A');
+	else if (j > 1 && j < x && i == 1)
+		ft_putchar('B');
+	else if (j == x && i == 1)
+		ft_putchar('C');
+	else if (i > 1 && i < y && !(j > 1 && j < x))
+		ft_putchar('B');
+	else if (i > 1 && i < y && (j > 1 || j < x))
+		ft_putchar(' ');
+	else if (j == 1 && i == y)
+		ft_putchar('A');
+	else if (j > 1 && j < x && i == y)
+		ft_putchar('B');
+	else if (j == x && i == y)
+		ft_putchar('C');
+	if (j == x)
+		ft_putchar('\n');
+}
+
+void	colle(int x, int y)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 1;
+	while (i <= y)
+	{
+		while (j <= x)
+		{
+			ft_draw_square(x, y, i, j);
+			j++;
+		}
+		j = 1;
+		i++;
+	}
+}
