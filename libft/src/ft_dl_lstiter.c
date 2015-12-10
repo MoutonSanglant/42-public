2807ebe016aa12709b608507792165a0957cb9d9
Thierry DEFRESNE <tdefresn@e3r4p5.42.fr>
2015-12-10 13:01:06 +0100

    iter: apply before iterations

--- libft/src/ft_dl_lstiter.c
+++ libft/src/ft_dl_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 16:28:38 by tdefresn          #+#    #+#             */
-/*   Updated: 2015/12/07 16:28:40 by tdefresn         ###   ########.fr       */
+/*   Updated: 2015/12/10 12:58:40 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_dl_lstiter(t_dl_list *lst, void (*f)(t_dl_list *elem))
 {
+	f(lst);
 	if (lst->next)
 		ft_dl_lstiter(lst->next, f);
-	f(lst);
 }
