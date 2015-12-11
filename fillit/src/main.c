e6c0e722a1e3a2f760c80fc4cfab85140751083f
MoutonSanglant <defresne.thierry@gmail.com>
2015-12-11 15:00:39 +0100

    feel it !

--- fillit/src/main.c
+++ fillit/src/main.c
@@ -6,53 +6,61 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 15:19:26 by tdefresn          #+#    #+#             */
-/*   Updated: 2015/12/11 02:07:27 by tdefresn         ###   ########.fr       */
+/*   Updated: 2015/12/11 14:11:05 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "fillit.h"
 
 /*
-** Optimal buff size is: size of (one tetriminos + 5 *\n) * max (546)
+** pattern ids
+** 0 -> square
+** 1 -> h-bar
+** 2 -> v-bar
+** 3 -> h-s
+** 4 -> v-s
+** 5 -> h-flipped-s
+** 6 -> v-flipped-s
+** 7 -> l
+** 8 -> l-90
+** 9 -> l-180
+** 10 -> l-270
+** 11 -> flipped-l
+** 12 -> flipped-l-90
+** 13 -> flipped-l-180
+** 14 -> flipped-l-270
+** 15 -> T
+** 16 -> T-90
+** 17 -> T-180
+** 18 -> T-270
 */
-//#define BUFSIZE 512
-#define BUFSIZE 546
 
-void	fillit_error(void)
-{
-	ft_putendl("error");
-	exit(1);
-}
-
-void	read_file(char *path)
-{
-	int		fd;
-	int		bcount;
-	char	buffer[BUFSIZE];
-
-	fd = open(path, O_RDONLY);
-	if (fd)
-	{
-		bcount = read(fd, buffer, BUFSIZE);
-		if (bcount > 0 && bcount < BUFSIZE)
-		{
-			buffer[bcount] = '\0';
-			
-			//ft_putendl(buffer);
-		}
-		else
-			fillit_error();
-	}
-	else
-		fillit_error();
-}
+char *g_pattern_table[18][5] = {
+	{ "##", "##", NULL },
+	{ "####", NULL },
+	{ "#", "#", "#", "#", NULL },
+	{ ".##", "##.", NULL },
+	{ "#.", "##", ".#", NULL },
+	{ "##.", ".##", NULL },
+	{ ".#", "##", "#.", NULL },
+	{ "#.",	"#.", "##", NULL },
+	{ "###", "#..", NULL },
+	{ "##",	".#", ".#", NULL },
+	{ "..#", "###", NULL },
+	{ ".#",	".#", "##", NULL },
+	{ "#..", "###", NULL },
+	{ "##",	"#.", "#.", NULL },
+	{ "###", "..#", NULL }
+};
 
 int		main(int argc, char **argv)
 {
+	char *file_buffer;
 	if (argc != 2)
-		fillit_error();
-	read_file(argv[1]);
+		error();
+	file_buffer = read_tetri_file(argv[1]);
+	if (!file_buffer)
+		error();
+	get_tetriminos_from_buffer(file_buffer);
 	return (0);
 }
