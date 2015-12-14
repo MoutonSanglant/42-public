2ac21e0ffe2b4d0cf5f45b8efb0d1e72bbd4990c
MoutonSanglant <defresne.thierry@gmail.com>
2015-12-14 12:37:04 +0100

    minor changes

--- fillit/src/errors.c
+++ fillit/src/errors.c
@@ -1,16 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   buffer_error.c                                     :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/11 14:42:46 by tdefresn          #+#    #+#             */
-/*   Updated: 2015/12/11 14:42:47 by tdefresn         ###   ########.fr       */
+/*   Created: 2015/12/14 12:15:46 by tdefresn          #+#    #+#             */
+/*   Updated: 2015/12/14 12:15:52 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+void	error(void)
+{
+	ft_putendl("error");
+	exit(1);
+}
 
 void	buffer_error(char *buffer)
 {
