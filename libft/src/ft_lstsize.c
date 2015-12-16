ad4adf4ed440839b4ea6f1a19554247b2739c0bc
MoutonSanglant <defresne.thierry@gmail.com>
2015-12-16 13:43:01 +0100

    fillit

--- libft/src/ft_lstsize.c
+++ libft/src/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 17:14:37 by tdefresn          #+#    #+#             */
-/*   Updated: 2015/12/14 11:48:25 by tdefresn         ###   ########.fr       */
+/*   Updated: 2015/12/16 12:12:30 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 int		ft_lstsize(t_list *lst)
 {
-	t_list	*first;
 	int		i;
 
-	first = lst;
 	i = 0;
-	while (first->next)
+	while (lst)
 	{
-		first = first->next;
 		i++;
+		lst = lst->next;
 	}
 	return (i);
 }
