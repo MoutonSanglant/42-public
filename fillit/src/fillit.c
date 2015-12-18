2ca7ff0e1694b1080b6cb65642496d076cb475c2
MoutonSanglant <defresne.thierry@gmail.com>
2015-12-18 15:57:54 +0100

    fillit

--- fillit/src/fillit.c
+++ fillit/src/fillit.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/13 17:05:24 by tdefresn          #+#    #+#             */
-/*   Updated: 2015/12/17 20:16:29 by tdefresn         ###   ########.fr       */
+/*   Updated: 2015/12/18 15:55:38 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void	print_grid(t_mask bounds, size_t w, t_list *final_list)
 
 	list_size = ft_lstsize(final_list);
 	bit = 1;
-	i = 0;
+	i = 1;
 	while (i <= w * w)
 	{
 #if DEBUG == 1
@@ -59,7 +59,7 @@ void	print_grid(t_mask bounds, size_t w, t_list *final_list)
 		else if (bit_in_list(bit, final_list, list_size) == 0)
 			ft_putchar('.');
 
-		if (i > 0 && (i % w == 0))
+		if (i > 0 && (i % (w) == 0))
 			ft_putchar('\n');
 #else
 		if (i > w && (i <= w * w) && (i % w == 0))
@@ -143,33 +143,31 @@ void	dellist(void *content, size_t content_size)
 
 #include <stdio.h>
 
-t_mask	bruteforce(t_list *tetri_list, t_mask grid, size_t grid_size, t_mask bounds, t_list *final_list)
+t_mask	bruteforce(t_list *tetri_list, t_mask grid, size_t grid_size, t_list *final_list)
 {
 	t_tetrimino		*tetri;
 	int				id;
 	t_mask			shift;
 	t_mask			tetri_mask;
 	t_mask			shifted_tetri_mask;
-	t_mask			forbidden_mask;
 	t_mask			tmp;
 	t_mask			tmp2;
 
 	// right & bottom mask
 	t_mask			right_mask;
 	t_mask			bottom_mask;
-	size_t i;
+	t_mask		last_mask;
+	size_t		i;
 
 	right_mask = 0;
 	bottom_mask = 0;
-	i = 0;
-	while (++i < grid_size * grid_size + grid_size)
-		if (i % grid_size == 0)
-			right_mask += ullpow(2, i);
-	i = 0;
-	while (++i < grid_size * grid_size + grid_size)
-		if (i > (grid_size * grid_size - grid_size))
-			bottom_mask += ullpow(2, i);
+	i = 0; // skip the 0
+	while (++i <= grid_size)
+		right_mask += ullpow(2, grid_size * i - 1);
 
+	i = 0;
+	while (i++ < grid_size )
+		bottom_mask += ullpow(2, grid_size * grid_size - grid_size + i);
 
 	tmp2 = grid;
 #if DEBUG == 1
@@ -178,6 +176,7 @@ t_mask	bruteforce(t_list *tetri_list, t_mask grid, size_t grid_size, t_mask boun
 	tetri = (t_tetrimino *)tetri_list->content;
 	id = (int)tetri->pattern_id;
 
+	shifted_tetri_mask = 0;
 	final_list->content = malloc(sizeof(t_mask));
 	if (final_list->content)
 		final_list->content_size = sizeof(t_mask);
@@ -185,69 +184,64 @@ t_mask	bruteforce(t_list *tetri_list, t_mask grid, size_t grid_size, t_mask boun
 	{
 		grid = tmp2;
 		tetri_mask = convert_to(g_mask_table[id][0], grid_size - 4);
-		forbidden_mask = grid | bounds;
-		//forbidden_mask = grid | right_mask;
-		shift = grid_size;
+		shift = grid_size * tetri->v_shift + tetri->h_shift;
 		shifted_tetri_mask = tetri_mask << shift;
-		//tetri_mask <<= shift;
-		//final_list = ft_lstnew((void const *)&(shifted_tetri_mask), sizeof(t_mask));
 		if (final_list->content)
 			final_list->content = ft_memcpy(final_list->content, (void const *)&(shifted_tetri_mask), sizeof(t_mask));
-		//while (OVERLAP(forbidden_mask, tetri_mask))
-		while (OVERLAP(forbidden_mask, shifted_tetri_mask))
-		//while (OVERLAP(grid, shifted_tetri_mask))
-		{
-#if DEBUG == 1
-			ft_putchar('_');
-#endif
-			shift = grid_size + grid_size * tetri->v_shift + tetri->h_shift;
-			shifted_tetri_mask = tetri_mask << shift;
-			if (final_list->content)
-				final_list->content = ft_memcpy(final_list->content, (void const *)&(shifted_tetri_mask), sizeof(t_mask));
-	
-			/*
-			** VOICI CE QUE JE DOIS FAIRE:
-			** remplacer le masque 'bounds' par une logique
-			** plus subtile
-			*/
-			//if (OVERLAP(right_mask, shifted_tetri_mask))
-			//	tetri->h_shift = tetri->h_shift + tetri->w;
-			//else
-			//	tetri->h_shift++;
-			//	ft_putchar('~');
 		
-			tetri->h_shift++;
-			if (tetri->h_shift > grid_size)
-			{
-			if (OVERLAP(right_mask, shifted_tetri_mask))
-				ft_putchar('~');
+		while (1)
+		{
+			shift = grid_size * tetri->v_shift + tetri->h_shift;
+			shifted_tetri_mask = tetri_mask << shift;
 
-				tetri->v_shift++;
-				tetri->h_shift = 0;
-				if (tetri->v_shift >= grid_size)
+			if (OVERLAP(grid, shifted_tetri_mask))
+			{
+#if DEBUG == 1
+				ft_putchar('_');
+#endif
+	
+				tetri->h_shift++;
+			}
+			else
+			{
+				last_mask = shifted_tetri_mask >> grid_size;
+				if (OVERLAP(bottom_mask, last_mask) && OVERLAP(bottom_mask, shifted_tetri_mask))
 				{
 #if DEBUG == 1
-					ft_putchar('$');
+					ft_putchar('&');
 #endif
 					tetri->v_shift = 0;
-					tetri->h_shift = 0;		
+					tetri->h_shift = 0;
 					return (0);
-					//ft_putendl("grid too small");
-					//error();
 				}
+				last_mask = shifted_tetri_mask >> 1;
+				if (OVERLAP(right_mask,last_mask) && OVERLAP(right_mask, shifted_tetri_mask))
+				{
+#if DEBUG == 1
+					ft_putchar('=');
+#endif
+					tetri->h_shift = 0;
+					tetri->v_shift++;
+					//final_list->content = NULL;
+
+					continue ;
+				}
+				if (final_list->content)
+						final_list->content = ft_memcpy(final_list->content, (void const *)&(shifted_tetri_mask), sizeof(t_mask));
+
+				tetri->h_shift++;
+				break ;
 			}
-			// TODO: logic when grid size is 7
 		}
 		grid |= shifted_tetri_mask;
 	
 		if (tetri_list->next)
 		{
 			final_list->next = ft_lstnew(NULL, 0);
-			tmp = bruteforce(tetri_list->next, grid, grid_size, bounds, final_list->next);
+			tmp = bruteforce(tetri_list->next, grid, grid_size, final_list->next);
 			if (tmp == 0)
 			{
 				ft_lstdel(&final_list->next, &dellist);
-//	ft_putchar('%');
 				continue ;
 			}
 			grid = tmp;
@@ -275,8 +269,8 @@ void	fillit(t_list *tetri_list)
 	// + 1 for the bounds
 	// Note: grids can be 7*7 max, so the biggest number
 	// of tetriminos in a regular grid is 12 (perfect fit)
-	grid_size = biggest_square(ft_lstsize(tetri_list) * 4) + 1;
-	//grid_size = biggest_square(ft_lstsize(tetri_list) * 4);
+	//grid_size = biggest_square(ft_lstsize(tetri_list) * 4) + 1;
+	grid_size = biggest_square(ft_lstsize(tetri_list) * 4);
 	
 	while (!tmp)
 	{
@@ -284,19 +278,6 @@ void	fillit(t_list *tetri_list)
 		bounds = 0;
 		i = -1;
 		
-		//*
-		if (grid_size <= 7)
-		{
-			while (++i < grid_size * grid_size + grid_size)
-				if (i < grid_size || i >= (grid_size * grid_size) || i % grid_size == 0)
-					bounds += ullpow(2, i);
-		}
-		else
-			while (++i < grid_size * grid_size + grid_size)
-				if (i < grid_size || i % grid_size == 0)
-					bounds += ullpow(2, i);
-		//*/
-
 #if DEBUG == 1
 		ft_putchar('!');
 		ft_putnbr(grid_size);
@@ -304,7 +285,7 @@ void	fillit(t_list *tetri_list)
 		if (grid_size > 8)
 			break ;
 		final_list = ft_lstnew(NULL, 0);
-		tmp = bruteforce(tetri_list, grid, grid_size, bounds, final_list);
+		tmp = bruteforce(tetri_list, grid, grid_size, final_list);
 		if (tmp == 0)
 		{
 			ft_lstdel(&final_list, &dellist);
@@ -317,16 +298,5 @@ void	fillit(t_list *tetri_list)
 	if (!final_list)
 		error();
 
-	/*
-	i = 0;
-	while (++i < grid_size * grid_size + grid_size)
-		if (i % grid_size == 0)
-			bounds += ullpow(2, i);
-	i = 0;
-	while (++i < grid_size * grid_size + grid_size)
-		if (i > (grid_size * grid_size - grid_size))
-			bounds += ullpow(2, i);
-	*/
-	//print_grid(bounds, grid_size, NULL);
-	print_grid(bounds, grid_size, final_list);
+	print_grid(0, grid_size, final_list);
 }
