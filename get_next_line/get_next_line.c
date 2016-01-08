beccd80bad94f6f33f2189239e43d70bcb7a853d
MoutonSanglant <defresne.thierry@gmail.com>
2016-01-08 06:51:23 +0100

    gnl + libft (fixes)

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/02 12:28:41 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/01/07 17:38:55 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/01/08 06:46:25 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,193 +14,141 @@
 
 #include "get_next_line.h"
 
-// 8 lignes
-static t_list		*new_parser(int fd)
+static int		clear_fd_parser(t_list **parser_list, int fd)
 {
-	t_parser	parser;
+	t_list	*fd_list;
+	t_list	*prev;
 
-	parser.fd = fd;
-	parser.bcount = 0;
-	ft_putendl("malloc: parser.buffer");
-	parser.buffer = (char *)ft_memalloc(sizeof(char) * BUFF_SIZE + 1);
-	if (!parser.buffer)
-		return (NULL);
-	ft_putendl("malloc: parser");
-	ft_putendl("malloc: parser.content");
-	return (ft_lstnew((void *)&parser, sizeof(parser)));
+	prev = NULL;
+	fd_list = *parser_list;
+	while (((t_parser *)fd_list->content)->fd != fd)
+	{
+		prev = fd_list;
+		fd_list = fd_list->next;
+		// ??
+		if (!fd_list)
+			return (-1);
+	}
+	if (prev && fd_list->next && prev->next)
+		prev->next = fd_list->next;
+	if (fd_list == (*parser_list))
+		*parser_list = (*parser_list)->next;
+	ft_memdel((void **)&((t_parser *)fd_list->content)->buffer);
+	ft_memdel((void **)&fd_list->content);
+	ft_memdel((void **)&fd_list);
+	return (0);
 }
 
-// 15 lignes
-static t_parser		*get_fd_parser(t_list *parser_list, int fd)
+static t_parser	*get_fd_parser(t_list **s_parsers, int fd)
 {
-	t_parser	*parser;
+	t_list		*parser_list;
+	t_parser	p;
 
-	parser = (t_parser *)parser_list->content;
-	while (parser->fd != fd)
+	p.fd = fd;
+	p.bcount = 0;
+	if (!*s_parsers && (!(p.buffer = (char *)ft_memalloc(BUFF_SIZE + 1))
+		|| !(*s_parsers = ft_lstnew((void *)&p, sizeof(p)))))
 	{
-		if (!parser_list->next)
+		if (p.buffer)
+			ft_memdel((void **)&p.buffer);
+		return (NULL);
+	}
+	parser_list = *s_parsers;
+	while (((t_parser *)parser_list->content)->fd != fd)
+	{
+		if (!parser_list->next
+				&& (!(p.buffer = (char *)ft_memalloc((BUFF_SIZE + 1)))
+				|| !(parser_list->next = ft_lstnew((void *)&p, sizeof(p)))))
 		{
-			parser_list->next = new_parser(fd);
-			parser = (t_parser *)parser_list->next->content;
-			break ;
+			if (p.buffer)
+				ft_memdel((void **)&p.buffer);
+			return (NULL);
 		}
 		parser_list = parser_list->next;
-		parser = (t_parser *)parser_list->content;
 	}
-	return (parser);
+	return ((t_parser *)parser_list->content);
 }
 
-// 38 lignes
-static size_t	get_line(t_parser *parser, t_list **strings)
+static int	to_eol(t_list **s, t_parser *p, size_t *total_bcount)
 {
-	char	*eol;
-	size_t	total_bcount;
-	t_list	*lst;
-	size_t	truncated_bytes;
+	size_t	eol;
 
-	lst = NULL;
-	eol = NULL;
-	total_bcount = 0;
-	while (!eol)
+	if (p->bcount == 0
+		&& (!(p->bcount = read(p->fd, p->buffer, BUFF_SIZE))))
+		return (1);
+	*total_bcount += p->bcount;
+	if (!*s)
+		*s = ft_lstnew(NULL, 0);
+	else
+		*s = (*s)->next;
+	(*s)->content = (char *)ft_memalloc(p->bcount + 1);
+	if (!(*s)->content)
+		return (-1);
+	(*s)->content_size = p->bcount;
+	if ((eol = (size_t)ft_memccpy((*s)->content,
+									(void *)p->buffer, '\n', p->bcount)))
 	{
-		if (parser->bcount == 0)
-		{
-			parser->bcount = read(parser->fd, parser->buffer, BUFF_SIZE);
-			if (parser->bcount == 0)
-				break;
-		}
-		//parser->buffer[parser->bcount] = '\0';
-		total_bcount += parser->bcount;
-		if (!*strings)
-		{
-			ft_putendl("malloc: strings");
-			*strings = ft_lstnew(NULL, 0);
-
-		}
-		if (!lst)
-			lst = *strings;
-		ft_putendl("malloc: strings.content");
-		(*strings)->content = (char *)ft_memalloc(sizeof(char) * (parser->bcount + 1));
-		(*strings)->content_size = parser->bcount;
-		if (!(*strings)->content)
-		{
-			ft_putendl("LEAKING\n\nHERE");
-			return (0);
-		}
-
-		eol = ft_memccpy((*strings)->content, (void *)parser->buffer, '\n', parser->bcount);
-		if (eol)
-		{
-			truncated_bytes = (int)eol - (int)(*strings)->content;
-			((char *)(*strings)->content)[truncated_bytes - 1] = '\0';
-			parser->bcount -= truncated_bytes;
-			parser->buffer = (char *)ft_memmove((void *)parser->buffer, (void *)&(parser->buffer[truncated_bytes]), parser->bcount);
-			break;
-		}
-		parser->bcount = 0;
-		ft_putendl("malloc: strings.next");
-		if ((*strings)->next)
-		{
-			ft_putendl("FREE\n\nLEAK");
-			*strings = lst;
-			return (0);
-		}
-		(*strings)->next = ft_lstnew(NULL, 0);
-		*strings = (*strings)->next;
-		(*strings)->next = NULL;
-		(*strings)->content = NULL;
-		(*strings)->content_size = 0;
+		eol -= (size_t)(*s)->content;
+		p->bcount -= eol;
+		((char *)(*s)->content)[eol - 1] = '\0';
+		p->buffer = (char *)ft_memmove((void *)p->buffer,
+										(void *)&(p->buffer[eol]), p->bcount);
+		return (1);
 	}
-	*strings = lst;
+	p->bcount = 0;
+	return (0);
+}
+
+static int	get_fd_line(char **line, t_list **s_parsers, int fd, t_list **strings)
+{
+	t_list		*first;
+	t_parser	*parser;
+	size_t		total_bcount;
+	int			r;
+
+	r = 0;
+	total_bcount = 0;
+	first = NULL;
+	if (!(parser = get_fd_parser(s_parsers, fd)))
+		return (-1);
+	while (!r)
+	{
+		if ((r = to_eol(strings, parser, &total_bcount)) && r < 0)
+			return (-1);
+		if (!first && *strings)
+			first = *strings;
+		if (r == 0)
+			(*strings)->next = ft_lstnew(NULL, 0);
+	}
+	*strings = first;
+	if (*line)
+		ft_memdel ((void **)&(*line));
+	if (!(*line = (char *)ft_memalloc(total_bcount + 1)))
+		return (-1);
 	return (total_bcount);
 }
 
-// 30 lignes
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*s_parsers = NULL;
-	t_parser		*parser; // optional
 	t_list			*strings;
-	t_list			*tmp_lst;
-	t_list			*last;
-	size_t			line_length;
+	t_list			*prev_str;
+	int				r;
 
-	parser = NULL;
+	r = 0;
 	strings = NULL;
-	if (!s_parsers && !(s_parsers = new_parser(fd)))
-		return (-1);
-	parser = get_fd_parser(s_parsers, fd);
-	if ((line_length = get_line(parser, &strings)))
+	if ((r = get_fd_line(line, &s_parsers, fd, &strings)))
 	{
-		if (*line)
-		{
-			ft_putendl("memdel: line");
-			ft_memdel ((void *)line);
-		}
-		ft_putendl("malloc: line");
-		*line = (char *)ft_memalloc(sizeof(char) * line_length + 1);
-		if (!*line)
-			return (-1);
-		//*line[0] = '\0';
 		while (strings)
 		{
-			tmp_lst = strings;
-			//ft_strncat(*line, (char *)strings->content, strings->content_size);
 			ft_strcat(*line, (char *)strings->content);
+			prev_str = strings;
 			strings = strings->next;
-			ft_putendl("memdel: strings->content");
-			ft_memdel ((void **)&tmp_lst->content);
-			ft_putendl("memdel: strings");
-			ft_memdel ((void **)&tmp_lst);
+			ft_memdel ((void **)&prev_str->content);
+			ft_memdel ((void **)&prev_str);
 		}
-		//(void) last;
-		//free(s_parsers);
-		//s_parsers = NULL;
 	}
 	else
-	{
-		/*
-		** Remove an element from the list
-		*/
-		while (strings)
-		{
-			ft_putendl("LEAKING\n\n\nSTRING");
-			tmp_lst = strings;
-			//ft_strncat(*line, (char *)strings->content, strings->content_size);
-			//ft_strcat(*line, (char *)strings->content);
-			strings = strings->next;
-			ft_putendl("memdel: strings->content");
-			ft_memdel ((void **)&tmp_lst->content);
-			ft_putendl("memdel: strings");
-			ft_memdel ((void **)&tmp_lst);
-		}
-		//free(s_parsers);
-		//s_parsers = NULL;
-		t_list *toto;
-		last = NULL;
-		tmp_lst = s_parsers;
-		while (((t_parser *)tmp_lst->content)->fd != fd)
-		{
-			last = tmp_lst;
-			ft_putendl("TMP_LST CHANGED");
-			tmp_lst = tmp_lst->next;
-			if (!tmp_lst)
-				return (-1);
-		}
-		if (last && tmp_lst->next && last->next)
-			last->next = tmp_lst->next;
-		toto = s_parsers;
-		if (tmp_lst == s_parsers)
-			s_parsers = s_parsers->next;
-		ft_putendl("memdel: s_parsers.content.buffer");
-		ft_memdel((void **)&((t_parser *)tmp_lst->content)->buffer);
-		ft_putendl("memdel: s_parsers.content");
-		ft_memdel((void **)&tmp_lst->content);
-		ft_putendl("memdel: s_parsers");
-		ft_memdel((void **)&tmp_lst);
-		if (!s_parsers && !tmp_lst)
-			ft_putendl("FULLY\n\n\nFLUSHED");
-		return (0);
-	}
-	return (1);
+		r = clear_fd_parser(&s_parsers, fd);
+	return (r);
 }
