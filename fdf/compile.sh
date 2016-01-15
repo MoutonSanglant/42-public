c5a352caeb9ce85871ebafdb318d0df07ef09137
MoutonSanglant <defresne.thierry@gmail.com>
2016-01-15 11:12:08 +0100

    minilibX sources

--- fdf/compile.sh
+++ fdf/compile.sh
@@ -3,4 +3,4 @@
 # osx
 # gcc -Wall -Werror -Wextra -I./libft main.c draw.c draw_3dgrid.c -L./lib -lmlx -L./libft -lft -framework OpenGL -framework AppKit
 # linux
-gcc -Wall -Werror -Wextra -o fdf -D LINUX -I./libft src/main.c src/draw.c src/draw_3dgrid.c -L./lib -lmlx_linux -lXext -lX11 -lm -lbsd -L./libft -lft
+gcc -Wall -Werror -Wextra -o fdf -D LINUX -I./libft src/main.c src/draw.c src/draw_3dgrid.c -L./lib -lmlx_x86linux -lXext -lX11 -lm -lbsd -L./libft -lft
