3728ab3289d37eb3dd0a4aecb094645ad8bb464b
MoutonSanglant <thierry.defresne@gmail.com>
2016-01-21 02:27:09 +0100

    fdf working matrixless pseudo-projection

--- fdf/src/draw_3dgrid.c
+++ fdf/src/draw_3dgrid.c
@@ -6,64 +6,104 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/18 17:52:02 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/01/20 03:14:30 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/01/21 01:47:00 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	draw_joints(t_mlx_sess *p, t_vector3 v, t_vector3 v2, t_matrix4 *mvp)
+#include <stdio.h>
+
+static int		rasterize_coord(t_mlx_sess *p, t_vector3 in, t_vector2 *out)
 {
-	t_vector2	from;
-	t_vector2	to;
-	t_vector4	vertex;
-	(void) to;
-	(void) from;
-	(void) p;
-	(void) mvp;
+	// Coords are in screen coodinate
+	// 1 - Pass to NDC coordinates
+	// 2 - Pass to raster coordinates
+(void) p;
 
-	vertex.x = v.x;
-	vertex.y = v.y;
-	vertex.z = v.z;
-	vertex.w = 1.f;
-	apply_matrix4(vertex, mvp);
-	/*if (vertex.w > 0)
-	{
-		if (vertex.x > 0)
-			vertex.x /= vertex.w;
-		if (vertex.y > 0)
-			vertex.y /= vertex.y;
-		if (vertex.z > 0)
-			vertex.z /= vertex.z;
-	}*/
-	//vertex.w /= vertex.w;
-	from.x = vertex.x;
-	from.y = vertex.y;
+	// This is a point in Camera space
+	out->x = in.x / -in.z;
+	out->y = in.y / -in.z;
 
-	vertex.x = v2.x;
-	vertex.y = v2.y;
-	vertex.z = v2.z;
-	vertex.w = 1.f;
+	if (ceil(ABS(out->x)) > p->canvasW * .5f || ceil(ABS(out->y)) > p->canvasH * .5f)
+		return (1);
+
+	// convert point coordinates to raster coordinates
+
+	// normalize x & y (set in [0,1] range)
+	out->x = (out->x + p->canvasW * .5f) / p->canvasW;
+	out->y = (out->y + p->canvasH * .5f) / p->canvasH;
+	// Coords are now in NDC space
+
+	// Pass to rang [0,x] & [0,y]
+	out->x = floor(out->x * p->width);
+	out->y = floor((1 - out->y) * p->width); // invert y
+
+	return (0);
+}
+
+static int draw_point(t_mlx_sess *p, t_vector3 vertex, t_vector2 *point, t_matrix4 *mvp)
+{
 	vertex = apply_matrix4(vertex, mvp);
-	/*if (vertex.w > 0)
-	{
-		if (vertex.x > 0)
-			vertex.x /= vertex.w;
-		if (vertex.y > 0)
-			vertex.y /= vertex.y;
-		if (vertex.z > 0)
-			vertex.z /= vertex.z;
-	}*/
-	//vertex.w /= vertex.w;
-	to.x = vertex.x;
-	to.y = vertex.y;
-	//ft_putnbr(from.y);
+	vertex = apply_matrix4(vertex, p->worldToCamera);
+	//vertex = apply_matrix4(vertex, p->projection);
+	//point = rasterize_coord(p, vertex);
+	if (rasterize_coord(p, vertex, point))
+		return (0);
 
-	if (from.x < 0 || from.x >= 1000 || from.y < 0 || from.y >= 1000)
+	//ft_putstr("z (view): ");
+	//ft_putnbr((*mvp)[11]);
+	//ft_putchar('\n');
+	//ft_putendl("==============");
+	if ((int)point->x < 0 || (int)point->x >= p->width || (int)point->y < 0 || (int)point->y >= p->height)
+		return (0);
+
+	draw_line((t_mlx_sess *)p, point, point);
+
+	return (1);
+}
+
+//static void	draw_joints(t_mlx_sess *p, t_vector3 v, t_vector3 v2, t_matrix4 *p->world, t_matrix4 *W2C)
+static void		draw_triangle(t_mlx_sess *p, t_triangle *triangle, t_matrix4 *mvp)
+{
+	t_vector3	a;
+	t_vector3	b;
+	t_vector3	c;
+	//t_vector3	vertices[3];
+	t_vector2	pixels[3];
+
+	a = triangle->a;
+	b = triangle->b;
+	c = triangle->c;
+	if (draw_point(p, a, &pixels[0], mvp) && draw_point(p, b, &pixels[1], mvp) && draw_point(p, c, &pixels[2], mvp))
+	{
+		p->col = 0x00ffffff;
+		draw_line((t_mlx_sess *)p, &pixels[0], &pixels[1]);
+		draw_line((t_mlx_sess *)p, &pixels[1], &pixels[2]);
+		draw_line((t_mlx_sess *)p, &pixels[2], &pixels[0]);
+	}
+
+	/*
+	vertices[0] = apply_matrix4(a, mvp);
+	vertices[0] = apply_matrix4(a, p->worldToCamera);
+	//vertex = apply_matrix4(a, p->projection);
+	if (rasterize_coord(p, vertices[0], &pixels[0]))
 		return ;
-	if (to.x < 0 || to.x >= 1000 || to.y < 0 || to.y >= 1000)
+	vertices[1] = apply_matrix4(b, mvp);
+	vertices[1] = apply_matrix4(b, p->worldToCamera);
+	if (rasterize_coord(p, vertices[1], &pixels[1]))
 		return ;
-	draw_line((t_mlx_sess *)p, &from, &to);
+	vertices[2] = apply_matrix4(c, mvp);
+	vertices[2] = apply_matrix4(c, p->worldToCamera);
+	if (rasterize_coord(p, vertices[2], &pixels[2]))
+		return ;
+*/
+	/*
+	if ((int)from.x <= 0 || (int)from.x >= p->width || (int)from.y <= 0 || (int)from.y >= p->height)
+		return ;
+	if ((int)to.x <= 0 || (int)to.x >= p->width || (int)to.y <= 0 || (int)to.y >= p->height)
+		return ;
+	*/
 }
 
 void	draw_3dgrid(t_mlx_sess *p)
@@ -71,56 +111,158 @@ void	draw_3dgrid(t_mlx_sess *p)
 //	static t_vector3	from;
 //	static t_vector3	to;
 
-	int		x = 0;
-	int		y = 0;
 	t_matrix4	mvp;
 	t_matrix4	model;
-
-	identity_matrix4(&mvp);
-	multiply_matrix4(&mvp, p->view);
-	multiply_matrix4(&mvp, p->projection);
-
-	// draw gizmo
+	static t_matrix4	*trans = NULL;
+	static int r = 0;
+	t_matrix4	rot;
+	t_matrix4	tmp;
+	t_vector3 s;
 	t_vector3 o;
-	t_vector3 ox;
-	t_vector3 oy;
-	t_vector3 oz;
+	//t_vector3 ox;
+	//t_vector3 oy;
+	//t_vector3 oz;
+
+	identity_matrix4(&model);
+	// Scale
+	//matrix4_product(&model, &tmp);
+	s.x = 50.f;
+	s.y = 50.f;
+	s.z = 50.f;
+	scaling_matrix4(&tmp, s);
+	matrix4_product(&model, &tmp);
+
+	// Rotate
+	// Translate
+	o.x = 0.f;
+	o.y = 0.f;
+	o.z = -1.f;
+	translation_matrix4(&tmp, o);
+	matrix4_product(&model, &tmp);
+
+	// Transformation matrix = world * view * projection;
+	// model
+	identity_matrix4(&mvp);
+	matrix4_product(&mvp, &model);
+	// world
+	// * view
+	//matrix4_product(&model, p->view);
+	// * projection
+	matrix4_product(&mvp, p->world);
+	//matrix4_product(&mvp, p->view);
+	//matrix4_product(&mvp, p->projection);
+
+	/*
+	// draw gizmo
 	o.x = 0.f;
 	o.y = 0.f;
 	o.z = 0.f;
-	ox.x = 100.f;
+	ox.x = 1.f;
 	ox.y = 0.f;
 	ox.z = 0.f;
 	p->col = 0x00ff0000;
-	draw_joints(p, o, ox, &mvp);
+	//draw_joints(p, o, ox, p->world, &model);
+	//draw_joints(p, o, ox, &mvp);
 	oy.x = 0.f;
-	oy.y = 100.f;
+	oy.y = 1.f;
 	oy.z = 0.f;
 	p->col = 0x0000ff00;
-	draw_joints(p, o, oy, &mvp);
+	//draw_joints(p, o, oy, p->world, &model);
+	//draw_joints(p, o, oy, &mvp);
 	oz.x = 0.f;
 	oz.y = 0.f;
-	oz.z = 100.f;
+	oz.z = -1.f;
 	p->col = 0x0000ffff;
-	(void) oz;
-	draw_joints(p, o, oz, &mvp);
+	//draw_joints(p, o, oz, p->world, &model);
+	//draw_joints(p, o, oz, &mvp);
+	*/
 	p->col = 0x00ffffff;
-
-	identity_matrix4(&model);
-	identity_matrix4(&mvp);
-	o.x = 100.f;
-	translation_matrix4(&model, o);
-	//rotationY_matrix4(&model, RAD(30));
-	multiply_matrix4(&mvp, &model);
-	multiply_matrix4(&mvp, p->view);
-	multiply_matrix4(&mvp, p->projection);
-
-	while (x < 9)
+	if (!trans)
 	{
-		while (y < 9)
+		trans = (t_matrix4 *)malloc(sizeof(t_matrix4));
+		identity_matrix4(trans);
+	}
+	identity_matrix4(&model);
+	// Scale
+	s.x = 100.f;
+	s.y = 100.f;
+	s.z = 100.f;
+	scaling_matrix4(&tmp, s);
+	matrix4_product(&model, &tmp);
+
+	// Rotate
+	// Translate
+	o.x = 17.f;
+	o.y = 0.f;
+	o.z = -1.f;
+	translation_matrix4(&tmp, o);
+	matrix4_product(&model, &tmp);
+
+	o.x = -1000.f;
+	o.y = -1000.f;
+	o.z = 10.f;
+	translation_matrix4(&tmp, o);
+	//matrix4_product(trans, &tmp);
+
+	// Rotate
+	rotationZ_matrix4(&rot, RAD(r));
+	//matrix4_product(&model, &rot);
+	r += 5;
+
+	// Model Matrix: Local
+	identity_matrix4(&mvp);
+	matrix4_product(&mvp, &model);
+	//ft_putstr("z (local): ");
+	//ft_putnbr(mvp[11]);
+	//ft_putendl(" (should be -1)");
+	// World (local-to-world)
+	matrix4_product(&mvp, p->world);
+	//ft_putstr("z (world): ");
+	//ft_putnbr(mvp[11]);
+	//ft_putendl(" (should be -1)");
+	// Camera (world-to-camera)
+	// we need the inverse of View
+	//t_matrix4 iview;
+
+	//inverse_matrix4(p->view, &iview);
+	//matrix4_product(&mvp, &iview);
+	//ft_putstr("z (view): ");
+	//ft_putnbr(mvp[11]);
+	//ft_putchar('\n');
+
+
+	// Transformation matrix = world * view * projection;
+	// model
+	//matrix4_product(&model, p->projection);
+	// world
+	// * view
+	//matrix4_product(&model, p->view);
+	// * projection
+	//matrix4_product(&mvp, p->projection);
+	p->col = 0x00ff0000;
+
+	//draw_triangle(p, &p->grid->triangles[100], &mvp);
+
+	int		i;
+	int		x = 0;
+	int		y = 0;
+
+	while (x < 12)
+		draw_triangle(p, &p->cube[x++], &mvp);
+
+	i = 0;
+	x = 0;
+	p->col = 0x00ffffff;
+	//matrix4_product(&mvp, &model);
+	while (x < p->grid->width)
+	{
+		while (y < p->grid->height)
 		{
-			draw_joints(p, p->grid[x + y * 10], p->grid[x + y * 10 + 1], &mvp);
-			draw_joints(p, p->grid[x + y * 10], p->grid[x + y * 10 + 10], &mvp);
+			draw_triangle(p, &p->grid->triangles[i++], &mvp);
+			draw_triangle(p, &p->grid->triangles[i++], &mvp);
+			//draw_joints(p, p->grid[x + y * 10], p->grid[x + y * 10 + 10], &mvp);
+			//draw_joints(p, p->grid[x + y * 10], p->grid[x + y * 10 + 1], p->world, &model);
+			//draw_joints(p, p->grid[x + y * 10], p->grid[x + y * 10 + 10], p->world, &model);
 			y++;
 		}
 		y = 0;
--- fdf/src/init_grid.c
+++ fdf/src/init_grid.c
@@ -6,27 +6,121 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/18 20:00:56 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/01/19 16:15:46 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/01/21 01:40:45 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	init_grid(t_vector3 *grid)
+void	cube(t_triangle *cube)
+{
+	t_vector3 points[8];
+
+	points[0].x = -.5f;
+	points[0].y = -.5f;
+	points[0].z = -.5f;
+	points[1].x = .5f;
+	points[1].y = -.5f;
+	points[1].z = -.5f;
+	points[2].x = .5f;
+	points[2].y = .5f;
+	points[2].z = -.5f;
+	points[3].x = -.5f;
+	points[3].y = .5f;
+	points[3].z = -.5f;
+	points[4].x = -.5f;
+	points[4].y = -.5f;
+	points[4].z = .5f;
+	points[5].x = .5f;
+	points[5].y = -.5f;
+	points[5].z = .5f;
+	points[6].x = .5f;
+	points[6].y = .5f;
+	points[6].z = .5f;
+	points[7].x = -.5f;
+	points[7].y = .5f;
+	points[7].z = .5f;
+
+	cube[0].a = points[0];
+	cube[0].b = points[1];
+	cube[0].c = points[3];
+	cube[1].a = points[1];
+	cube[1].b = points[2];
+	cube[1].c = points[3];
+	cube[2].a = points[1];
+	cube[2].b = points[5];
+	cube[2].c = points[2];
+	cube[3].a = points[5];
+	cube[3].b = points[6];
+	cube[3].c = points[2];
+	cube[4].a = points[5];
+	cube[4].b = points[4];
+	cube[4].c = points[6];
+	cube[5].a = points[4];
+	cube[5].b = points[7];
+	cube[5].c = points[6];
+	cube[6].a = points[4];
+	cube[6].b = points[0];
+	cube[6].c = points[7];
+	cube[7].a = points[0];
+	cube[7].b = points[3];
+	cube[7].c = points[7];
+	cube[8].a = points[3];
+	cube[8].b = points[2];
+	cube[8].c = points[7];
+	cube[9].a = points[2];
+	cube[9].b = points[6];
+	cube[9].c = points[7];
+	cube[10].a = points[0];
+	cube[10].b = points[1];
+	cube[10].c = points[4];
+	cube[11].a = points[1];
+	cube[11].b = points[5];
+	cube[11].c = points[4];
+}
+
+void	init_grid(t_grid *grid, int width, int height)
 {
 	int		x;
 	int		y;
+	int		i;
+
 	x = 0;
 	y = 0;
-	while (x < 10)
+	i = 0;
+	grid->width = width;
+	grid->height = height;
+
+	// The number of triangles in the grid is
+	// faces * 2
+	// which is equals to
+	// (width * height) * 2
+	grid->triangles = (t_triangle *)malloc(sizeof(t_triangle) * width * height * 2);
+	while (x < width)
 	{
-		while(y < 10)
+		while(y < height)
 		{
-			grid[x + y * 10].x = (double)(50.f * x);
-			grid[x + y * 10].y = (double)(50.f * y);
-			//grid[x + y * 10].z = (double)(50 * y);
-			//grid[x + y * 10].y = 0.0;
-			grid[x + y * 10].z = 0.f;
+			grid->triangles[i].a.x = (float)(1.f * x);
+			grid->triangles[i].a.y = (float)(1.f * y);
+			grid->triangles[i].a.z = 0.f;
+			grid->triangles[i].b.x = (float)(1.f * x + 1.f);
+			grid->triangles[i].b.y = (float)(1.f * y);
+			grid->triangles[i].b.z = 0.f;
+			grid->triangles[i].c.x = (float)(1.f * x);
+			grid->triangles[i].c.y = (float)(1.f * y + 1.f);
+			grid->triangles[i].c.z = 0.f;
+			i++;
+
+			grid->triangles[i].a.x = (float)(1.f * x + 1.f);
+			grid->triangles[i].a.y = (float)(1.f * y);
+			grid->triangles[i].a.z = 0.f;
+			grid->triangles[i].b.x = (float)(1.f * x + 1.f);
+			grid->triangles[i].b.y = (float)(1.f * y + 1.f);
+			grid->triangles[i].b.z = 0.f;
+			grid->triangles[i].c.x = (float)(1.f * x);
+			grid->triangles[i].c.y = (float)(1.f * y + 1.f);
+			grid->triangles[i].c.z = 0.f;
+			i++;
 			y++;
 		}
 		y = 0;
