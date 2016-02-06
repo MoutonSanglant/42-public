078abca8cf4eeabcc11ef74c9d205b303ba298cb
MoutonSanglant <defresne.thierry@gmail.com>
2016-02-06 09:06:51 +0100

    strsplit fix

--- libft/src/ft_strsplit.c
+++ libft/src/ft_strsplit.c
@@ -6,18 +6,18 @@
 /*   By: tdefresn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 12:36:01 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/01/26 22:36:44 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/02/06 01:40:44 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		**set(char **table, int idx, char *str)
+static char		**set(char **table, int idx, char *new_str)
 {
 	int i;
 
 	i = 0;
-	table[idx] = str;
+	table[idx] = new_str;
 	if (!table[idx])
 	{
 		while (i < idx)
@@ -27,35 +27,30 @@ static char		**set(char **table, int idx, char *str)
 	return (table);
 }
 
-/*
-** TODO: CRASH
-** count += (*s) ? 1 : 0;
-** line 52
-*/
-
 static char		**split(char const *s, char c, size_t count)
 {
 	char	**words_table;
 	char	*str;
+	int		idx;
 
-	while (*s == c)
+	idx = 0;
+	while (s[idx] == c)
 		s++;
-	str = ft_strchr(s, c);
+	str = ft_strchr(&s[idx], c);
 	if (!str)
 	{
-		words_table = (char **)ft_memalloc(sizeof(char *)
-							* (count + 1));
-		if (words_table)
-			words_table = set(words_table, count,
-								ft_strsub(s, 0, ft_strlen(s)));
-		if (words_table)
+		count += (s[0] == '\0') ? 0 : 1;
+		if ((words_table = (char **)ft_memalloc(sizeof(char *) * (count + 1))))
+		{
 			words_table[count] = NULL;
+			words_table = set(words_table, count - 1,
+								ft_strsub(&s[idx], 0, ft_strlen(&s[idx])));
+		}
 		return (words_table);
 	}
-	words_table = split(str, c, count + 1);
-	if (words_table)
-		words_table = set(words_table, count,
-							ft_strsub(s, 0, ((uintptr_t)str - (uintptr_t)s)));
+	if ((words_table = split(str, c, count + 1)))
+		words_table = set(words_table, count, ft_strsub(&s[idx], 0,
+								((uintptr_t)str - (uintptr_t)&s[idx])));
 	return (words_table);
 }
 
