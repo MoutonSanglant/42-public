98b3370e5f21b1f211f340b0cc8d3704b12fd74c
Thierry DEFRESNE <tdefresn@e2r5p16.42.fr>
2016-03-05 20:46:54 +0100

    new: lstsort, debug_error_free

--- libft/src/ft_lstadd.c
+++ libft/src/ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 10:30:18 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/01/26 19:42:12 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/03/05 20:19:10 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_lstadd(t_list **alst, t_list *new)
 {
 	if (!alst || !new)
 	{
-		ft_putendl("ft_lstadd error:\nsent parameter is NULL.");
+		ERROR_PARAM("ft_lstadd");
 		return ;
 	}
 	if (!*alst)
 	{
-		ft_putendl("ft_lstadd error:\n candidate for free is NULL.");
+		ERROR_FREE("ft_lstadd");
 		return ;
 	}
 	new->next = *alst;
--- libft/src/ft_memccpy.c
+++ libft/src/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 12:37:29 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/01/26 20:00:11 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/03/05 19:58:45 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ void	*ft_memccpy(void *dst, void const *src, int c, size_t n)
 
 	if (!dst || !src)
 	{
-		ft_putendl("ft_memccpy returned the following error:\n\
-					sent parameter is NULL.\n\
-					Verify your code, it is unsafe !!!");
+		ERROR_PARAM("ft_memccpy");
 		return (NULL);
 	}
 	i = 0;
