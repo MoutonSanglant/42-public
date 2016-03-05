069e42f72740ba7c25009cda07392084bc72ac82
Thierry DEFRESNE <tdefresn@e2r5p16.42.fr>
2016-03-05 17:49:53 +0100

    catch arguments, open directory

--- ls/src/main.c
+++ ls/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/03 17:43:51 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/03/03 18:19:49 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/03/05 17:46:23 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,16 +44,100 @@
 **		exit
 */
 
+/*
+**	2 steps routine:
+**	- read flags
+**	- read paths
+*/
+
+static int		read_flags(int argc, char **argv, t_ls_datas *ls_datas)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	*arg;
+
+	i = 0;
+	while (++i < argc && argv[i][0] == '-')
+	{
+		arg = argv[i];
+		len = ft_strlen(arg);
+		if (len < 2)
+			break ;
+		j = 0;
+		while (++j < len)
+		{
+			if (arg[j] == 'R')
+				ls_datas->flags |= FLAG_BIG_R;
+			else if (arg[j] == 'a')
+				ls_datas->flags |= FLAG_A;
+			else if (arg[j] == 'l')
+				ls_datas->flags |= FLAG_L;
+			else if (arg[j] == 'r')
+				ls_datas->flags |= FLAG_R;
+			else if (arg[j] == 't')
+				ls_datas->flags |= FLAG_T;
+			else
+				error_usage(arg[j]);
+		}
+	}
+	return (i - 1);
+}
+
+static void		read_args(int argc, char **argv, t_ls_datas *ls_datas)
+{
+	t_list	*path;
+	char	*tmp;
+	char	*arg;
+	int		i;
+
+	i = read_flags(argc, argv, ls_datas);
+	while (++i < argc)
+	{
+		arg = argv[i];
+		if ((tmp = ft_strdup(arg)))
+		{
+			path = ft_lstnew((void *)tmp, sizeof(char *) * (ft_strlen(tmp) + 1));
+			if (ls_datas->path)
+				ft_lstadd(&ls_datas->path, path);
+			else
+				ls_datas->path = path;
+			ft_strdel(&tmp);
+		}
+	}
+	if (!ls_datas->path)
+		ls_datas->path = ft_lstnew((void *)".", sizeof(char *) * 2);
+}
+
+void	read_dir(t_list *path)
+{
+	DIR		*p_dir;
+	struct dirent	*p_dirent;
+
+	p_dir = opendir((char *)path->content);
+	if (!p_dir)
+	{
+		ft_printf("Cannot open directory: %s\n", (char *)path->content);
+		return ;
+		//error();
+	}
+	ft_printf("%s: \n", (char *)path->content);
+	while ((p_dirent = readdir(p_dir)))
+		ft_printf("%s\n", p_dirent->d_name);
+	closedir(p_dir);
+}
+
 int		main(int argc, char **argv)
 {
-	char	*path;
+	t_ls_datas		ls_datas;
+	t_list			*path;
 
-	if (argc < 2 || argv[argc - 1][0] == '-')
-		path = ft_strdup(".");
-	else
-		path =  ft_strjoin("./", argv[argc - 1]);
-
-	ft_putendl("ft_ls is your friend.");
-	ft_printf("path: %s\n", path);
+	read_args(argc, argv, &ls_datas);
+	path = ls_datas.path;
+	while (path)
+	{
+		read_dir(path);
+		path = path->next;
+	}
 	return (0);
 }
