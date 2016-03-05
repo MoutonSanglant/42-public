d98c4b2c4333d6eb9e341244deef9d9fba70c3a0
MoutonSanglant <defresne.thierry@gmail.com>
2016-03-05 20:48:23 +0100

    sorting prototype

--- ls/src/errors.c
+++ ls/src/errors.c
@@ -6,21 +6,22 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/05 17:15:49 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/03/05 17:45:41 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/03/05 19:34:43 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
 #include "ft_ls.h"
 
-void	error_errno()
+int		error_path(const char *s)
 {
-	ft_printf("Error: %i\n", errno);
-	exit(errno);
+	ft_putstr("ft_ls: ");
+	perror(s);
+	return (errno);
 }
 
 void	error_usage(int c)
 {
 	ft_printf("ft_ls: illegal option -- %c\nusage: ./ft_ls [-%s] [file ...]\n", c, VALID_FLAGS);
-	exit(-1);
+	exit(1);
 }
