32c94cb683dce2671085bc4801a8abaec48f800b
MoutonSanglant <defresne.thierry@gmail.com>
2016-03-15 23:36:37 +0100

    norme ft_lstsort

--- libft/src/ft_lstsort.c
+++ libft/src/ft_lstsort.c
@@ -6,43 +6,44 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/05 19:01:17 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/03/09 12:45:20 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/03/15 21:53:21 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstsort(t_list *lst, int (*cmp)(void *, void *))
+static int	sort(t_list **first, int (*cmp)(void *, void *))
 {
-	t_list	*first;
+	t_list	*lst;
 	t_list	*next;
 	t_list	*prev;
 	int		sorted;
 
-	sorted = 0;
-	first = lst;
-	while (!sorted)
+	sorted = 1;
+	prev = *first;
+	lst = *first;
+	next = lst->next;
+	while (lst->next)
 	{
-		sorted = 1;
-		prev = first;
-		lst = first;
-		next = lst->next;
-		while (lst->next) //lst->next
+		if (!cmp(lst->content, lst->next->content) && !(sorted = 0))
 		{
-			if (!cmp(lst->content, lst->next->content)) // lst->next
-			{
-				sorted = 0;
-				if (lst == first)
-					first = lst->next; // lst->next
-				if (prev != lst)
-					prev->next = lst->next; // lst->next
-				lst->next = next->next; //lst->next
-				next->next = lst; // lst->next
-			}
-			prev = lst;
-			lst = next; // lst->next
-			next = lst->next;
+			if (lst == *first)
+				*first = lst->next;
+			if (prev != lst)
+				prev->next = lst->next;
+			lst->next = next->next;
+			next->next = lst;
 		}
+		prev = lst;
+		lst = next;
+		next = lst->next;
 	}
-	return (first);
+	return (sorted);
+}
+
+t_list		*ft_lstsort(t_list *lst, int (*cmp)(void *, void *))
+{
+	while (!sort(&lst, cmp))
+		;
+	return (lst);
 }
